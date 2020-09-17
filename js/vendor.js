ea5526ce201e341a36ab546df3fd4663a18d2864
Стас Штауб <shtaubstas@gmail.com>
2020-09-17 17:53:22 +0300

    Update 2020-09-17T14:53:16.589Z

---
+++ js/vendor.js
@@ -0,0 +1,1 @@
+"use strict";var _interopRequireDefault=require("@babel/runtime/helpers/interopRequireDefault");require("@babel/polyfill");var _svg4everybody=_interopRequireDefault(require("svg4everybody"));(0,_svg4everybody.default)(),require("ninelines-ua-parser");
