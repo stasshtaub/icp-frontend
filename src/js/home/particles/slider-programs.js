22c48584314c09f0477eb120d6d45ddb145b183d
Стас Штауб <shtaubstas@gmail.com>
2020-09-28 01:48:28 +0300

    fix/ Исправления слайдера программ

--- src/js/home/particles/slider-programs.js
+++ src/js/home/particles/slider-programs.js
@@ -1,7 +1,6 @@
 const init = () => {
 	const swiper = new Swiper(".swiper-container", {
 		slidesPerView: "auto",
-		spaceBetween: -210,
 		loop: true,
 		loopedSlides: 2,
 		centeredSlides: true,
@@ -9,13 +8,10 @@ const init = () => {
 			nextEl: ".programs-carousel__btn--next",
 			prevEl: ".programs-carousel__btn--prev",
 		},
-		breakpoints: {
-			1090: {
-				spaceBetween: -90,
-			},
-			1300: {
-				spaceBetween: 0,
-			},
+		pagination: {
+			el: ".programs-carousel__dots",
+			bulletClass: "programs-carousel__dot",
+			clickable: true,
 		},
 	});
 };
