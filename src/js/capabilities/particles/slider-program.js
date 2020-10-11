2d2f0401f9a7fe55aef53d13c4ae842333add864
Стас Штауб <shtaubstas@gmail.com>
2020-10-11 17:21:34 +0300

    feature/ Сверстал слайдер "Программа курса"

--- src/js/capabilities/particles/slider-program.js
+++ src/js/capabilities/particles/slider-program.js
@@ -2,5 +2,9 @@
 	new Swiper(".capabilities-program__slider", {
 		slidesPerView: 'auto',
 		freeMode: true,
+		navigation: {
+			nextEl: ".capabilities-program__btn--next",
+			prevEl: ".capabilities-program__btn--prev",
+		},
 	});
 }
