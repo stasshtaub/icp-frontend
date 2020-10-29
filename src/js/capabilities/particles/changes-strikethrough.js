49aece6a72f0a40c9763fcfef3b64cfc88cb3b48
Стас Штауб <shtaubstas@gmail.com>
2020-10-29 18:46:27 +0300

    feature/ Начал перписывать скрипт зачеркивания

--- src/js/capabilities/particles/changes-strikethrough.js
+++ src/js/capabilities/particles/changes-strikethrough.js
@@ -1,25 +1,30 @@
 const intersectionObserver = new IntersectionObserver((entries) => {
 	entries.forEach(({ isIntersecting, target }) => {
 		if (isIntersecting) {
-			const box = target.getBoundingClientRect();
-			const top = box.top + pageYOffset;
-			console.log("top:", top);
+			const lines = target.querySelectorAll(
+				".capabilities-changes__strikethrough-line"
+			);
 
-			window.addEventListener("scroll", onScroll);
-		} else {
-			window.removeEventListener("scroll", onScroll);
+			// Расстояние по высоте за которое линия должна быть полностью зачеркнута
+			const strikeHeight = innerHeight / lines.length;
+
+			function onScroll() {
+				lines.forEach((line, i) => {
+					const bottom = i * strikeHeight;
+
+					// Устанавливаем '--scroll' в зависимости от расстояния от верхнего края строки до верхнего края области зачеркивания для этой строки
+					// Когда расстояние между ними = 0: '--scroll' = 1
+					// Когда расстояние между ними = strikeHeight - высота строки: '--scroll' = 0
+				});
+			}
+
+			document.addEventListener("scroll", onScroll);
 		}
 	});
 });
 
-const onScroll = () => {
-	console.log("scroll");
-};
-
-const items = document.querySelectorAll(
-	".capabilities-changes__strikethrough-item"
+const container = document.querySelector(
+	".capabilities-changes__strikethrough"
 );
 
-items.forEach((item) => {
-	intersectionObserver.observe(item);
-});
+intersectionObserver.observe(container);
