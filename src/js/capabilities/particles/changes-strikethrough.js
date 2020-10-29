28b9c0bab44c429de18dc7e4497225f8f9afdd7a
Стас Штауб <shtaubstas@gmail.com>
2020-10-30 00:36:48 +0300

    feature/ Закончил анимацию

--- src/js/capabilities/particles/changes-strikethrough.js
+++ src/js/capabilities/particles/changes-strikethrough.js
@@ -1,30 +1,36 @@
+// const intersectionObserver = new IntersectionObserver((entries) => {
+// 	entries.forEach(({isIntersecting, target}) => {
+// 		function onScroll() {
+// 			const scrollValue =
+// 				1 - target.getBoundingClientRect().top / innerHeight;
+// 			target.style.setProperty('--scroll', scrollValue);
+// 		}
+// 		if (isIntersecting) {
+// 			document.addEventListener('scroll', onScroll);
+// 		} else {
+// 			document.removeEventListener('scroll', onScroll);
+// 		}
+// 	});
+// });
+
+// const container = document.querySelector(
+// 	'.capabilities-changes__strikethrough',
+// );
+
+// intersectionObserver.observe(container);
+
 const intersectionObserver = new IntersectionObserver((entries) => {
-	entries.forEach(({ isIntersecting, target }) => {
+	entries.forEach(({isIntersecting, target}) => {
 		if (isIntersecting) {
-			const lines = target.querySelectorAll(
-				".capabilities-changes__strikethrough-line"
+			target.classList.add(
+				'capabilities-changes__strikethrough--animated',
 			);
-
-			// Расстояние по высоте за которое линия должна быть полностью зачеркнута
-			const strikeHeight = innerHeight / lines.length;
-
-			function onScroll() {
-				lines.forEach((line, i) => {
-					const bottom = i * strikeHeight;
-
-					// Устанавливаем '--scroll' в зависимости от расстояния от верхнего края строки до верхнего края области зачеркивания для этой строки
-					// Когда расстояние между ними = 0: '--scroll' = 1
-					// Когда расстояние между ними = strikeHeight - высота строки: '--scroll' = 0
-				});
-			}
-
-			document.addEventListener("scroll", onScroll);
 		}
 	});
 });
 
 const container = document.querySelector(
-	".capabilities-changes__strikethrough"
+	'.capabilities-changes__strikethrough',
 );
 
 intersectionObserver.observe(container);
