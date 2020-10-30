7f54d78a53df9fe5e46479e4213fb82bf5be4d7f
Стас Штауб <shtaubstas@gmail.com>
2020-10-30 15:23:47 +0300

    feature/ Добавил анимацию (2 варианта)

---
+++ src/js/capabilities/particles/shape-animation.js
@@ -0,0 +1,17 @@
+const shapeList = document.querySelector(".capabilities-more__list");
+
+const options = {
+    threshold: 1
+}
+
+const observer = new IntersectionObserver((entries, observer) => {
+	entries.forEach(({ isIntersecting, target }) => {
+		if (isIntersecting) {
+			target.classList.add("capabilities-more__list--animated");
+		} else {
+			target.classList.remove("capabilities-more__list--animated");
+		}
+	});
+}, options);
+
+observer.observe(shapeList);
