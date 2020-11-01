b29d73e4d85c633018211366ffabe910aa8ce52e
Стас Штауб <shtaubstas@gmail.com>
2020-11-01 15:57:25 +0300

    fix/ Добавил точки слайдера программ на главной, сделал ширину слайдов 100% на экранах <= 1024px (LG)

--- src/js/capabilities/particles/slider-program.js
+++ src/js/capabilities/particles/slider-program.js
@@ -6,5 +6,13 @@
 			nextEl: ".capabilities-program__btn--next",
 			prevEl: ".capabilities-program__btn--prev",
 		},
+		pagination: {
+			el: ".capabilities-program__dots",
+			bulletClass: "slider__dots-item",
+			bulletActiveClass: "slider__dots-item--active capabilities-program__dots-item--active",
+			renderBullet: (_, className) =>
+				`<li class="${className}"><div class="slider__dot capabilities-program__dot"></div></li>`,
+			clickable: true
+		},
 	});
 }
