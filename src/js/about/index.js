5ee9dd637a70f4634d5b086dd2942ec7578786ec
Стас Штауб <shtaubstas@gmail.com>
2020-11-23 02:00:19 +0300

    feature/ Добавил темный header

--- src/js/about/index.js
+++ src/js/about/index.js
@@ -1,2 +1,8 @@
 import "./particles/slider-reviews";
 import "./particles/slider-news";
+
+const header = document.querySelector(".header");
+
+if (header) {
+    header.classList.add("header--dark");
+}
