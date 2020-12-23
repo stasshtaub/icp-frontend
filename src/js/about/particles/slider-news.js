6f09fb069e7a73f8b07d67565cd0ef1e4e862ecc
Стас Штауб <shtaubstas@gmail.com>
2020-12-23 16:37:29 +0300

    feature/ Отсключил подгрузку в слайдере на странице "о центре"

--- src/js/about/particles/slider-news.js
+++ src/js/about/particles/slider-news.js
@@ -1,16 +1,17 @@
 {
 	const prevMask = document.querySelector(".about-news__nav-mask--left");
+	const nextMask = document.querySelector(".about-news__nav-mask--right");
 	const prevEl = document.querySelector(".about-news__nav-btn--prev");
 	const nextEl = document.querySelector(".about-news__nav-btn--next");
 
-	let lastId = document.querySelectorAll(".about-news__slide:not(.about-news__slide--loader)").length;
-	let limit = 10;
-	let loading = false;
+	// let lastId = document.querySelectorAll(".about-news__slide:not(.about-news__slide--loader)").length;
+	// let limit = 10;
+	// let loading = false;
 
-	const loadNews = async () => {
-		const response = await fetch(`https://jsonplaceholder.typicode.com/photos?_start=${lastId}&_limit=${limit}`);
-		return await response.json();
-	};
+	// const loadNews = async () => {
+	// 	const response = await fetch(`https://jsonplaceholder.typicode.com/photos?_start=${lastId}&_limit=${limit}`);
+	// 	return await response.json();
+	// };
 
 	const swiper = new Swiper(".about-news__slider", {
 		slidesPerView: "auto",
@@ -24,37 +25,44 @@
 				prevMask.classList.remove("about-news__nav-mask--visible");
 			},
 
-			slideNextTransitionStart: () => {
-				if (!prevMask.classList.contains("about-news__nav-mask--visible")) {
-					prevMask.classList.add("about-news__nav-mask--visible");
-				}
+			reachEnd: () => {
+				nextMask.classList.remove("about-news__nav-mask--visible");
 			},
 
-			slideNextTransitionEnd: async ({ slides, activeIndex }) => {
-				// 4 - количество оставшихся справа слайдов до подгрузки
-				if (!loading && slides.length >= limit && activeIndex >= slides.length - 4) {
-					loading = true;
-
-					const news = await loadNews();
-
-					news.forEach(({ title, url }) => {
-						const slide = `
-							<li class="about-news__slide swiper-slide">
-								<div class="about-news__card">
-									<img class="about-news__img" src="${url}" alt="${title}">
-									<p class="about-news__title">${title}</p>
-									<p class="about-news__description">${title} ${title} ${title}</p>
-									<p class="about-news__date">30 августа</p>
-								</div>
-							</li>
-						`;
-						swiper.addSlide(lastId - 1, slide);
-						lastId++;
-					});
-
-					loading = false;
+			fromEdge: ({ activeIndex }) => {
+				if (activeIndex === 0 && !prevMask.classList.contains("about-news__nav-mask--visible")) {
+					prevMask.classList.add("about-news__nav-mask--visible");
+				} else if (!nextMask.classList.contains("about-news__nav-mask--visible")) {
+					console.log("from end", activeIndex);
+					nextMask.classList.add("about-news__nav-mask--visible");
 				}
 			}
+
+			// slideNextTransitionEnd: async ({ slides, activeIndex }) => {
+			// 	// 4 - количество оставшихся справа слайдов до подгрузки
+			// 	if (!loading && slides.length >= limit && activeIndex >= slides.length - 4) {
+			// 		loading = true;
+
+			// 		const news = await loadNews();
+
+			// 		news.forEach(({ title, url }) => {
+			// 			const slide = `
+			// 				<li class="about-news__slide swiper-slide">
+			// 					<div class="about-news__card">
+			// 						<img class="about-news__img" src="${url}" alt="${title}">
+			// 						<p class="about-news__title">${title}</p>
+			// 						<p class="about-news__description">${title} ${title} ${title}</p>
+			// 						<p class="about-news__date">30 августа</p>
+			// 					</div>
+			// 				</li>
+			// 			`;
+			// 			swiper.addSlide(lastId - 1, slide);
+			// 			lastId++;
+			// 		});
+
+			// 		loading = false;
+			// 	}
+			// }
 		}
 	});
 }
