bd689a5b058312042f810872e32a1a92beae47d4
Стас Штауб <shtaubstas@gmail.com>
2021-01-18 13:44:47 +0300

    feature/ Добавлены data-hidden для тригеров и data-possible-hidden для формы

--- src/js/forms/forms-participation.js
+++ src/js/forms/forms-participation.js
@@ -7,30 +7,58 @@ const consentCheckbox = form.querySelector("[name='consent']");
 const submitButton = form.querySelector("[type='submit']");
 const loader = document.querySelector(".forms-participation .loader");
 
+const possibleHidden = form.dataset.possibleHidden && JSON.parse(form.dataset.possibleHidden);
+
 buttons.forEach((btn) => {
     btn.addEventListener("click", (e) => {
         e.preventDefault();
-        const tariff = btn.dataset.tariff || null;
-        
-        let tariffInput = form.querySelector("[name='tariff']");
 
-        if (tariffInput) {
-            if (tariff) {
-                tariffInput.value = tariff;
-                tariffInput.type = "hidden";
-                tariffInput.name = "tariff";
-            } else {
-                tariffInput.parentNode.removeChild(tariffInput);
-            }
-        } else {
-            if (tariff) {
-                tariffInput = document.createElement("input");
-                form.appendChild(tariffInput);
-                tariffInput.value = tariff;
-                tariffInput.type = "hidden";
-                tariffInput.name = "tariff";
+        if (Array.isArray(possibleHidden)) {
+            possibleHidden.forEach((name) => {
+                const el = form.querySelector(`[name='${ name }']`);
+                console.log(`[name='${ name }']`);
+                if (el) {
+                    el.parentElement.removeChild(el);
+                }
+            })
+        }
+
+        const hiddenFields = btn.dataset.hidden && JSON.parse(btn.dataset.hidden);
+        
+        if (hiddenFields) {
+            for (const name in hiddenFields) {
+                if (Array.isArray(possibleHidden) && possibleHidden.includes(name)) {
+                    const el = document.createElement("input");
+                    el.type = "hidden";
+                    el.value = hiddenFields[name];
+                    el.name = name;
+                    form.appendChild(el);
+                }
             }
         }
+
+        // // old
+        // const tariff = btn.dataset.tariff || null;
+        
+        // let tariffInput = form.querySelector("[name='tariff']");
+
+        // if (tariffInput) {
+        //     if (tariff) {
+        //         tariffInput.value = tariff;
+        //         tariffInput.type = "hidden";
+        //         tariffInput.name = "tariff";
+        //     } else {
+        //         tariffInput.parentNode.removeChild(tariffInput);
+        //     }
+        // } else {
+        //     if (tariff) {
+        //         tariffInput = document.createElement("input");
+        //         form.appendChild(tariffInput);
+        //         tariffInput.value = tariff;
+        //         tariffInput.type = "hidden";
+        //         tariffInput.name = "tariff";
+        //     }
+        // }
 
         modal.showModal()
     });
@@ -41,9 +69,10 @@ consentCheckbox.addEventListener("change", (e) => {
 });
 
 form.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
     loader.classList.add("loader--active");
 
-    e.preventDefault();
     const { action, method } = form;
 
     if (action) {
