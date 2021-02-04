8d34d2075ddf94970b512bbb7fd2ea88d4aaf3bd
Стас Штауб <shtaubstas@gmail.com>
2021-02-04 16:41:52 +0300

    feature/ Добавлена кнопка загрузки комментариев

---
+++ src/js/news-detail2/particles/comments-load.js
@@ -0,0 +1,9 @@
+const trigger = document.querySelector(".comments__more-btn");
+
+if (trigger) {
+    trigger.addEventListener("click", loadComments)
+}
+
+const loadComments = () => {
+    // TODO: Реализовать загрузку комментов, когда появится на беке
+}
