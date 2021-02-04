a4f2fa61476e7c831eaf4325f69eb565d2b541eb
Стас Штауб <shtaubstas@gmail.com>
2021-02-04 17:51:27 +0300

    feature/ Добавлен аватар в форме комментов

--- src/js/news-detail2/particles/comments.js
+++ src/js/news-detail2/particles/comments.js
@@ -104,3 +104,24 @@ const prepareFormData = (form) => {
 
 	return fd;
 };
+
+const avatarImg = document.querySelector(".comments-form__avatar-img");
+const avatarInput = document.querySelector(".comments-form__avatar-input");
+const avatarBtn = document.querySelector(".comments-form__avatar-label");
+
+const handleUpload = event => {
+  const file = event.target.files[0];
+  const reader = new FileReader();
+
+  reader.readAsDataURL(file);
+  reader.onloadend = () => {
+    const imageBuffer = reader.result;
+    avatarImg.style.background = `url(${imageBuffer}) center center/cover`;
+  };
+};
+
+avatarInput.addEventListener("change", handleUpload);
+avatarBtn.addEventListener("keypress", event => {
+  if (event.key === " " || event.key === "Enter") avatarBtn.click();
+});
+
