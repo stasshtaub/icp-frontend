bd1f60779c5fb9689023797f452ac9abea04c250
Стас Штауб <shtaubstas@gmail.com>
2021-03-02 02:06:46 +0300

    feature/ Измнено кадрирование картинок, добавлен отступ дате, добавлена проверка кнопки

--- src/js/news/particles/news-list.js
+++ src/js/news/particles/news-list.js
@@ -1,64 +1,67 @@
 {
 	const moreBtn = document.getElementById("news-more__btn");
-	const listContainer = document.querySelector(".news-list__list");
-	
-	let currentPage = 1;
-	let limit = 8;
-	let totalPages = 2;
 
-	const addNews = (img, title, description, date, url) => {
-		const artictle = `
+	if (moreBtn) {
+		const listContainer = document.querySelector(".news-list__list");
+	
+		let currentPage = 1;
+		let limit = 8;
+		let totalPages = 2;
+
+		const addNews = (img, title, description, date, url) => {
+			const artictle = `
 			<article class="news-card">
-				<a class="news-card__link" href="${ url }">
-					<img src="${ img }" alt="news image" class="news-card__img" />
+				<a class="news-card__link" href="${url}">
+					<img src="${img}" alt="news image" class="news-card__img" />
 					<div class="news-card__info">
-						<p class="news-card__title text text--s">${ title }</p>
-						<p class="news-card__description text text--s3">${ description }</p>
-						<p class="news-card__date text text--xs2">${ date }</p>
+						<p class="news-card__title text text--s">${title}</p>
+						<p class="news-card__description text text--s3">${description}</p>
+						<p class="news-card__date text text--xs2">${date}</p>
 					</div>
 				<a>
 			</article>
 		`;
 
-		const listItem = `<li class="news-list__item">${ artictle }</li>`;
+			const listItem = `<li class="news-list__item">${artictle}</li>`;
 
-		listContainer.insertAdjacentHTML("beforeend", listItem);
-	}
+			listContainer.insertAdjacentHTML("beforeend", listItem);
+		}
 
-	const fetchNews = async () => {
-		const response = await fetch(
-			"/local/templates/icpcenter/components/bitrix/news/news/bitrix/news.list/.default/ajax.php?" +
+		const fetchNews = async () => {
+			const response = await fetch(
+				"/local/templates/icpcenter/components/bitrix/news/news/bitrix/news.list/.default/ajax.php?" +
 				new URLSearchParams({
 					ajax: "y",
 					i_num_page: currentPage,
 					n_page_size: limit
 				})
-		);
+			);
 
-		return await response.json();
-	};
+			return await response.json();
+		};
 
-	const loadNews = async () => {
-		moreBtn.disabled = true;
+		const loadNews = async () => {
+			moreBtn.disabled = true;
 
-		currentPage++;
+			currentPage++;
 
-		const { response: { items, total_page_count } } = await fetchNews();
+			const { response: { items, total_page_count } } = await fetchNews();
 
-		items.forEach(({ img, name, description, prop_date, url }) => {
-			addNews(img, name, description, prop_date, url)
-		});
+			items.forEach(({ img, name, description, prop_date, url }) => {
+				addNews(img, name, description, prop_date, url)
+			});
 
-		if (totalPages !== total_page_count) {
-			totalPages = total_page_count;
-		}
+			if (totalPages !== total_page_count) {
+				totalPages = total_page_count;
+			}
 		
-		if (currentPage === totalPages) {
-			moreBtn.parentNode.removeChild(moreBtn);
+			if (currentPage >= totalPages) {
+				moreBtn.parentNode.removeChild(moreBtn);
+			}
+
+			moreBtn.disabled = false;
 		}
 
-		moreBtn.disabled = false;
+		moreBtn.addEventListener("click", loadNews)
 	}
-
-	moreBtn.addEventListener("click", loadNews)
 }
