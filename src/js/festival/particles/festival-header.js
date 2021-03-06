b84511c927c03a136c6c6936664d727e25049ebb
Стас Штауб <shtaubstas@gmail.com>
2021-03-06 15:01:23 +0300

    feature/ Свёрстан стартовый экран, добавлено скрытие шапки при скроле

--- src/js/festival/particles/festival-header.js
+++ src/js/festival/particles/festival-header.js
@@ -1,25 +1,41 @@
-const contactsEl = document.querySelector('.festival > .festival-contacts')
-const contacsContent = contactsEl?.querySelector('.festival-contacts__content')
+const contactsEl = document.querySelector(".festival > .festival-contacts");
+const contacsContent = contactsEl?.querySelector(".festival-contacts__content");
 
-const stickyClass = 'festival-contacts__content--sticky'
+const stickyClass = "festival-contacts__content--sticky";
+const activeClass = "festival-contacts__content--active";
 
 const initHeight = () => {
-    contactsEl.style.minHeight = `${ contacsContent.offsetHeight }px`
-}
+	contactsEl.style.minHeight = `${contacsContent.offsetHeight}px`;
+};
 
 if (contactsEl.getBoundingClientRect().top <= 0) {
-    contacsContent.classList.add(stickyClass)
-} 
+	contacsContent.classList.add(stickyClass);
+}
 
-const io = new IntersectionObserver(([entry]) => {
-    console.log(entry);
-    if (entry.boundingClientRect.top <= 0) {
-        contacsContent.classList.add(stickyClass)
-    } else {
-        contacsContent.classList.remove(stickyClass)
-    }
-}, { threshold: 1 })
+const io = new IntersectionObserver(
+	([entry]) => {
+		if (entry.boundingClientRect.top <= 0) {
+			contacsContent.classList.add(stickyClass);
+		} else {
+			contacsContent.classList.remove(stickyClass);
+		}
+	},
+	{ threshold: 1 }
+);
 
-initHeight()
-io.observe(contactsEl)
-window.addEventListener("resize", initHeight)
+initHeight();
+io.observe(contactsEl);
+window.addEventListener("resize", initHeight);
+
+let lastScrollTop = 0;
+window.addEventListener("scroll", () => {
+	const st = window.pageYOffset || document.documentElement.scrollTop;
+	if (st > lastScrollTop) {
+		contacsContent.classList.remove(activeClass);
+	} else {
+		if (contacsContent.classList.contains(stickyClass)) {
+			contacsContent.classList.add(activeClass);
+		}
+	}
+	lastScrollTop = st <= 0 ? 0 : st;
+});
