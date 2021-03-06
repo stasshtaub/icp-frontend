018dd250535de6ab65bfa7455c5026789a13f5d4
Stas Shtaub <shtaubstas@gmailcom>
2021-03-06 03:22:23 +0300

    refactor/ Переделана анимация шапки

--- src/js/festival/particles/festival-header.js
+++ src/js/festival/particles/festival-header.js
@@ -1,18 +1,25 @@
-const headerEl = document.querySelector('.festival-header')
 const contactsEl = document.querySelector('.festival > .festival-contacts')
+const contacsContent = contactsEl?.querySelector('.festival-contacts__content')
 
-if (headerEl && contactsEl) {
-    if (contactsEl.getBoundingClientRect().top <= 0) {
-        headerEl.classList.add('active')
-    } 
+const stickyClass = 'festival-contacts__content--sticky'
 
-    const io = new IntersectionObserver(([entry]) => {
-        if (entry.boundingClientRect.top <= 0) {
-            headerEl.classList.add('active')
-        } else {
-            headerEl.classList.remove('active')
-        }
-    }, { threshold: 1 })
-
-    io.observe(contactsEl)
+const initHeight = () => {
+    contactsEl.style.minHeight = `${ contacsContent.offsetHeight }px`
 }
+
+if (contactsEl.getBoundingClientRect().top <= 0) {
+    contacsContent.classList.add(stickyClass)
+} 
+
+const io = new IntersectionObserver(([entry]) => {
+    console.log(entry);
+    if (entry.boundingClientRect.top <= 0) {
+        contacsContent.classList.add(stickyClass)
+    } else {
+        contacsContent.classList.remove(stickyClass)
+    }
+}, { threshold: 1 })
+
+initHeight()
+io.observe(contactsEl)
+window.addEventListener("resize", initHeight)
